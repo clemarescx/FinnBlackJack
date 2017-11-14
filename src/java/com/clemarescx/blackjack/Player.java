e4aa96e7e48ba858ac4de690c5fc3e06b2e6f835
Clement Marescaux <marcle15@student.westerdals.no>
2017-11-14 18:34:21 +0100

    Implement player

--- src/java/com/clemarescx/blackjack/Player.java
+++ src/java/com/clemarescx/blackjack/Player.java
@@ -1,7 +1,34 @@
 package com.clemarescx.blackjack;
 
+import java.util.ArrayList;
+
 /**
  * Created by Clem on 13/11/2017.
  */
 public class Player {
+    String name;
+    ArrayList<Card> hand;
+    private int startWinScore;
+
+    public Player(String name, int startWinScore){
+        this.name = name;
+        this.startWinScore = startWinScore;
+        hand = new ArrayList<Card>();
+    }
+
+    protected void addCardToHand(Card c){
+        getHand().add(c);
+    }
+
+    protected void resetHand(){
+        getHand().clear();
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public int getStartWinScore() {
+        return startWinScore;
+    }
 }
